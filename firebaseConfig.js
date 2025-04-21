ecbff8628d1079b9c6b684839d7ad82d5177f1ae
Nagi.akc <samyakchiche04@gmail.com>
2025-04-21 18:40:30 +0100

    included firebaseConfig for github deployement

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBt_57xN3T3VlcKKakGb63fd-WhgY783fg",
+    authDomain: "onepiece-quiz-2022.firebaseapp.com",
+    projectId: "onepiece-quiz-2022",
+    storageBucket: "onepiece-quiz-2022.firebasestorage.app",
+    messagingSenderId: "693060014516",
+    appId: "1:693060014516:web:9f3e8d1ad15a4226f722e1",
+    measurementId: "G-36MJF9TJ90",
+};
